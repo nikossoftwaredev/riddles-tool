21c9bf074a503e8f26832ff43de6b46ae6f59a22
Nikos D <nikossoftwaredev@gmail.com>
2023-02-19 01:39:38 +0200

    improved search a bit

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -53,6 +53,11 @@ const SearchPage = () => {
 
       filteredStreets = filteredStreets.filter(({ street }) => isMatch(street));
     } else if (term) {
+      const tempFilteredStreets = [...filteredStreets];
+      filteredStreets = filteredStreets.filter(({ street }) => street.startsWith(term));
+
+      if (filteredStreets.length === 0) filteredStreets = [...tempFilteredStreets];
+
       filteredStreets = filteredStreets.filter(({ street }) => {
         const rgx = new RegExp(term, "ig");
         return rgx.test(street);
